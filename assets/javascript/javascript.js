a88054d052b4e271c0710233f2a281801c21e31b
Adrienne Schueller <atschueller@gmail.com>
2018-03-21 19:45:14 -0400

    fixed some stuff

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,31 +4,32 @@ var topics = ['horses', 'beagles', 'rock music', 'shopping'];
 makeButtons();
 
 //Make a function that creates buttons out of the array//
-function makeButtons(){
+function makeButtons() {
     $('#gifButtons').empty();
-for (i = 0; i < topics.length; i++) {
-    var buttons = $('<button>');
-    buttons.append(topics[i]);
-    $('#gifButtons').append(buttons);
-};
+    for (i = 0; i < topics.length; i++) {
+        var buttons = $('<button>');
+        buttons.append(topics[i]);
+        $('#gifButtons').append(buttons);
+    };
 };
 
 
-//Create an on click function to get the user input and make queryURL//
-$('#gifinput').on('click', function () {
-    var things = $(this).attr('data-name');
+//Create an on click function to show Giphys//
+$('#gifButtons').on('click', function () {
+    var things = $(this).attr('data');
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-        things + "&api_key=4fsb7d1ohaJA2VY1PVRh8MBdqYD7TAh1&limit=10";
+        things + "&api_key=4fsb7d1ohaJA2VY1PVRh8MBdqYD7TAh1&limit=10&offset=0&rating=PG&lang=en";
     $.ajax({
         url: queryUrl,
-        method: GET,
-    }).then(function (reponse) {
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
         var stuffiLike = response.data;
-        for (i = 0; i < response.length; i++) {
+        for (i = 0; i < stuffiLike.length; i++) {
             if (stuffiLike[i].rating === "pg") {
                 var gifDiv = $("<div class='gif'>");
                 var rating = stuffiLike[i].rating;
-                var para = $("<p>").text("Rating: " + rating);
+                var pRating = $("<p>").text("Rating: " + rating);
                 var stuffImage = $("<img>");
                 stuffImage.attr("src", stuffiLike[i].images.fixed_height.url);
                 gifDiv.append(para);
