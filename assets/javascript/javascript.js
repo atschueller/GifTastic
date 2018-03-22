e763bb1f4a11f8c66bfc5da351d650a6faeb8093
Adrienne Schueller <atschueller@gmail.com>
2018-03-22 16:15:11 -0400

    added some javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,41 +1,59 @@
 var topics = ['horses', 'beagles', 'rock music', 'shopping'];
+var searchInput = '';
+var queryURLBase = "https://api.giphy.com/v1/gifs/random?api_key=4fsb7d1ohaJA2VY1PVRh8MBdqYD7TAh1&l"
 
 
 makeButtons();
-
 //Make a function that creates buttons out of the array//
 function makeButtons() {
     $('#gifButtons').empty();
     for (i = 0; i < topics.length; i++) {
         var buttons = $('<button>');
+        buttons.attr('data');
         buttons.append(topics[i]);
         $('#gifButtons').append(buttons);
     };
 };
+//Create buttons out of user input in the search form//
+$('#gifInput').on('click', function (event) {
+    event.preventDefault();
+    searchInput = $('#gifInput').val().trim();
+    var gifSearch = queryURLBase + searchInput;
+    topics.push(gifSearch);
+    makeButtons(gifSearch);
+});
 
 
-//Create an on click function to show Giphys//
+//Make a click function that animates them//
+
+//Make a click function that makes them still//
+
+//Create a function to show Giphys//
 $('#gifButtons').on('click', function () {
-    var things = $(this).attr('data');
+    var things = $(this).attr("data");
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
         things + "&api_key=4fsb7d1ohaJA2VY1PVRh8MBdqYD7TAh1&limit=10&offset=0&rating=PG&lang=en";
     $.ajax({
         url: queryUrl,
         method: "GET",
     }).then(function (response) {
+        console.log(queryUrl);
         console.log(response);
         var stuffiLike = response.data;
         for (i = 0; i < stuffiLike.length; i++) {
-            if (stuffiLike[i].rating === "pg") {
-                var gifDiv = $("<div class='gif'>");
+            if (stuffiLike[i].rating === "pg" && stuffiLike[i].rating === "g") {
+                var gifDiv = $("<div class='item'>");
                 var rating = stuffiLike[i].rating;
-                var pRating = $("<p>").text("Rating: " + rating);
-                var stuffImage = $("<img>");
-                stuffImage.attr("src", stuffiLike[i].images.fixed_height.url);
-                gifDiv.append(para);
-                gifDiv.append(stuffImage);
-                $("#gifAdd").prepend(gifDiv);
+                var gifP = $('<p>').text('Rating:' + rating);
+                var image = stuffiLike[i].images;
+                var gifImage = $('<img>');
+                gifImage.attr("src", stuffiLike[i].images.fixed_height.url);
+
+                gifDiv.append(gifP);
+                gifDiv.append(gifImage);
+
+                $('#gifBody').prepend(gifDiv);
             };
         };
     });
-})
+});
