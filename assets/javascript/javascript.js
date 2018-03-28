f4f117444b2db5bb8d867d5794df01ca48490354
Adrienne Schueller <atschueller@gmail.com>
2018-03-28 00:03:10 -0400

    js updated

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,16 +1,17 @@
 var topics = ['horses', 'beagles', 'rock music', 'shopping'];
 var searchInput = '';
-var queryURLBase = "https://api.giphy.com/v1/gifs/random?api_key=4fsb7d1ohaJA2VY1PVRh8MBdqYD7TAh1&l"
-
-
+var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=4fsb7d1ohaJA2VY1PVRh8MBdqYD7TAh1&q=&limit=10&offset=0&rating=PG&lang=en"
+var gifDiv = $("<div>");
+var gifImage;
 makeButtons();
 //Make a function that creates buttons out of the array//
 function makeButtons() {
-    $('#gifButtons').empty();
+    $('.gifButtons').empty();
     for (i = 0; i < topics.length; i++) {
         var buttons = $('<button>');
-        buttons.attr('data');
+        buttons.data('topics', topics[i]);
         buttons.append(topics[i]);
+        buttons.addClass('gifButtons');
         $('#gifButtons').append(buttons);
     };
 };
@@ -21,16 +22,13 @@ $('#gifInput').on('click', function (event) {
     var gifSearch = searchInput;
     topics.push(gifSearch);
     makeButtons(gifSearch);
+    $('#gifSearch').empty();
 });
 
-
-//Make a click function that animates them//
-
-//Make a click function that makes them still//
-
-//Create a function to show Giphys//
-$('#gifButtons').on('click', function () {
-    var things = $(this).attr("data");
+//Create an on click function to show Giphys//
+$('.gifButtons').on('click', function () {
+    var things = $(this).data("topics");
+    console.log(things);
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
         things + "&api_key=4fsb7d1ohaJA2VY1PVRh8MBdqYD7TAh1&limit=10&offset=0&rating=PG&lang=en";
     $.ajax({
@@ -42,18 +40,36 @@ $('#gifButtons').on('click', function () {
         var stuffiLike = response.data;
         for (var i = 0; i < stuffiLike.length; i++) {
             if (stuffiLike[i].rating === "pg" || stuffiLike[i].rating === "g") {
-                var gifDiv = $("<div class='item'>");
                 var rating = stuffiLike[i].rating;
                 var gifP = $('<p>').text('Rating:' + rating);
                 var image = stuffiLike[i].images;
                 var gifImage = $('<img>');
-                gifImage.attr("src", stuffiLike[i].images.fixed_height.url);
-
+                gifImage.attr("src", stuffiLike[i].images.fixed_height_small_still.url);
+                gifImage.attr('data-still', stuffiLike[i].images.fixed_height_small_still.url);
+                gifImage.attr('data-animate', stuffiLike[i].images.fixed_height_small.url);
+                gifImage.attr('paused', stuffiLike[i].images.fixed_height_small_still.url)
+                gifDiv.addClass('giphy');
                 gifDiv.append(gifP);
                 gifDiv.append(gifImage);
+                
 
                 $('#gifBody').prepend(gifDiv);
             };
         };
     });
 });
+//Create an on click function to make Giphys animate or become still//
+$(document).on('click', '.giphy', function (event) {
+    var gifState = $(this).attr('data-state');
+    if (gifState === 'paused') {
+        var animateGiphy = $(this).attr("data-animate");
+        $(this).attr('src', $(this).attr('data-animate'));
+        $(this).attr('data-state', 'animate');
+    } else {
+        var stillGiphy = $(this).attr("data-still");
+        $(this).attr('src', $(this).attr('data-still'));
+        $(this).attr('data-state', 'data-still');
+    };
+});
+
+
